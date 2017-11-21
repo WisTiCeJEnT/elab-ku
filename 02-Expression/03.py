143ac1a85117b45426fac794e45e809bbb6ee439
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-11-21 14:34:28 +0700

    3-4

---
+++ 02-Expression/03.py
@@ -0,0 +1,9 @@
+name = "Arthur"
+print(f"|12345678901234567890|")
+print(f"|--------------------|")
+x = 12.3456789
+#ans is from here
+print(f"|{name:<20}|")
+print(f"|{name:>20}|")
+print(f"|{name:^20}|")
+print(f"|{x:>20.3f}|")
---
+++ 02-Expression/04.py
@@ -0,0 +1,2 @@
+cost = 50+60+125+300+80+300+90
+print("The discounted cost is {:3.2f}".format(cost*0.85))
