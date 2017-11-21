0e435db5a30ad2b5a8fbd046f7ddd9f0c6a1bdd9
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-11-21 14:10:18 +0700

    1-2

---
+++ 02-Expression/02.py
@@ -0,0 +1,7 @@
+#ex1
+year = int(input("Enter a year in A.D.: "))
+print("The year in B.E. is",year+543)
+#ex2
+w = float(input("Enter rectangle's width: "))
+h = float(input("Enter rectangle's height: "))
+print("The area of the rectangle is",w*h)
