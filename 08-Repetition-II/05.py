4aab45338f2c52877e0950f8835dc706d39e4ae8
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-12-07 22:47:48 +0700

    PPPP-

---
+++ 08-Repetition-II/05.py
@@ -0,0 +1,14 @@
+h = int(input("Enter the depth of the well: "))
+u = int(input("Enter the height the frog can jump: "))
+d = int(input("Enter the height the frog slips down: "))
+if u-d>0:
+    count = 1
+    while h>u:
+        h -= u
+        print(f"On day {count} the frog leaps to the depth of {h} meters.")
+        h += d
+        print(f"At night he slips down to the depth of {h} meters.")
+        count += 1
+    print(f"The frog can escape the well on day {count}.")
+else:
+    print("The frog will never escape from the well.")
