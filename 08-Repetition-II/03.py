1839db53d22fea5f0b4a94cf35e823bfa15c1f07
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-12-07 16:57:11 +0700

    P-PP-PP-

--- 08-Repetition-II/03.py
+++ 08-Repetition-II/03.py
@@ -1,7 +1,11 @@
 def fah_to_cel(start, end, step): # version 2
     print(f"{'Fahrenheit':>12}{'Celcius':>12}")
     print(f"{'----------':>12}{'-------':>12}")
-    for fah in range(start,end,step):
+    fah = start
+    while fah != end:
+        if (start>end and step>0) or (start<end and step<0):
+            break
         cel = (5/9)*(fah-32)
         print(f"{fah:12.2f}{cel:12.2f}")
+        fah = fah + step
     print(f"{'----------':>12}{'-------':>12}")
