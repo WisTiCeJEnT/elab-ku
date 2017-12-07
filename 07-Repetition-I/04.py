24f23d5377d3fbf741981cb62b4122dc32157dfd
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-12-07 15:04:03 +0700

    1 line 3 input

--- 07-Repetition-I/04.py
+++ 07-Repetition-I/04.py
@@ -1,5 +1,9 @@
+print((input()*int(input())+'\n')*int(input()),end='')
+"""
+#========== Just for fun ===============
 s = int(input("Enter block size: "))
 c = input("Character to use: ")
 c = c*s + '\n'
 c = c*s
 print(c,end='')
+"""
